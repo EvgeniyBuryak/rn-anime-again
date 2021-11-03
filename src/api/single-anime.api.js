697cd7e58f5375f86d5f4b32d20b869d092657d3
Jhonah <buryakone@gmail.com>
2021-11-03 18:14:55 +0700

    запрос конкретного анимэ

---
+++ src/api/single-anime.api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const getSingleAnime = async (id) => {
+
+    const requestSingleAnime = axios.create({
+        baseURL: 'https://kitsu.io/api/edge/anime',
+        header: { 'content-type': 'application/vnd.api+json' },
+        params: {
+            'filter[id]': id,
+        }
+    });
+
+    try {
+        const result = await requestSingleAnime.get();
+
+        return result.data.data;
+    } catch (err) {
+        return new Error(err);
+    }
+};
+
+export { getSingleAnime };
