70279ea9584d3cedde60215a1bbc756036513b3f
Jhonah <buryakone@gmail.com>
2021-11-03 18:15:20 +0700

    добавлен экран одного анимэ

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/home-screens/home-screens.view';
+import ResultsShowScreen from './src/screens/results-show-screen/results-show-screen.view';
 
 const navigator = createStackNavigator(
   {
-    'Home': HomeScreen,
+    Home: HomeScreen,
+    ResultsShow: ResultsShowScreen,
   },
   {
     initialRouteName: 'Home',
